f6de1c2959d229217b902e04b29483df67734f22
Satish Muraharisetti <sm251296@ncr.com>
2021-12-03 21:39:36 -0500

    Adding ability to download allure

---
+++ QaAutomation/openAllureReport.ps1
@@ -0,0 +1,11 @@
+﻿try{
+    $allureVersion = allures --version
+}
+catch {
+ Write-Host  "Install allure reports for windows"
+ Write-Host Here is the link to konow more about allure https://docs.qameta.io/allure/
+ exit
+}
+
+allure generate --clean
+allure open allure-report
